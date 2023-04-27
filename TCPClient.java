08443e3e93b9a5da34e141977eeaaeac9bb164bb
CallumMcDonald <61964490+CallumMcDonald@users.noreply.github.com>
2023-04-28 09:50:43 +1000

    Added comments for code clarity

--- TCPClient.java
+++ TCPClient.java
@@ -10,26 +10,33 @@ public class TCPClient {
      public static void main(String[] args) {
          try {
              int jobID = 0;
-
+            //create a new socket
             Socket s = new Socket("localhost", 50000);
+
+            //initialise the inputs readers and writers
             BufferedReader in = new BufferedReader(new
             InputStreamReader(s.getInputStream()));
             DataOutputStream out = new
             DataOutputStream(s.getOutputStream());
 
+            //send 'helo'
             out.write(("HELO\n").getBytes());
             out.flush();
             System.out.println("SENT: HELO");
 
+            //return recieved message
             String str = in.readLine();
             System.out.println("RCVD: "+str);
 
+            //return authorisation and username
             String username = System.getProperty("user.name");
             out.write(("AUTH " + username + "\n").getBytes());
-
+            
+            //read the response
             str = in.readLine();
             System.out.println("RCVD: " + str);
 
+            //send 'redy'
             out.write(("REDY\n").getBytes());
             out.flush();
             str = in.readLine();
@@ -41,11 +48,13 @@ public class TCPClient {
                 System.out.println("Job ID: " + jobID);
              }
 
+            //write 'quit'
             out.write(("QUIT\n").getBytes());
             out.flush();
             str = in.readLine();
             System.out.println("RCVD: " + str);
 
+            //close the socket
             in.close();
             out.close();
             s.close();
